783a26e8a7b854096bdf65e4610ae2a04a74abd6
Nara Tekchuen <nartek01@gmail.com>
2025-10-07 12:28:02 +0200

    style: add outline-none to search input

--- nara/src/component/search.tsx
+++ nara/src/component/search.tsx
@@ -1,7 +1,7 @@
 function search() {
   return (
     <>
-      <input placeholder='sök' className='border-2 border-green-300 rounded-m p-2'/>
+      <input placeholder='sök' className='border-2 border-green-300 rounded-m p-2 outline-none'/>
     </>
   )
 }
