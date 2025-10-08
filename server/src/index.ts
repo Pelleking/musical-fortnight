506a008ae51ae28e4691315fed09fdbd428ec36d
Pelle Fredrikson <pelle.fredrikson@academedia.se>
2025-10-08 14:20:24 +0200

    Added dogssearch endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
+import path from 'path'
 const app = express()
 const port = 3000
+
+const ENABLE_DOGS_SEARCH = false
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+if (ENABLE_DOGS_SEARCH) {
+  app.get('/dogssearch', (req, res) => {
+    res.send('TODO: dogssearch')
+  })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
