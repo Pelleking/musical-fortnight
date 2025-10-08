f1f46162946b8c07d40b19b211d44b04f768541b
Lucia Macakova <lucia.macakova@academedia.se>
2025-10-08 14:35:05 +0200

    chore: add zero test case

--- server/src/feature/productSearch/index.test.ts
+++ server/src/feature/productSearch/index.test.ts
@@ -1,5 +1,12 @@
 import { deepEqual } from 'node:assert/strict'
 import { test } from "node:test"
+import { productSearch } from ''
+
 test('it works', () => {
     deepEqual(0, 0);
+})
+
+test('zero test case', () => {
+    const result = productSearch('');
+    deepEqual(result, []);
 })
