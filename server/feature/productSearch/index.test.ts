60cb48515c20304a918dc711d0e89d5925411896
Lucia Macakova <lucia.macakova@academedia.se>
2025-10-08 14:28:30 +0200

    chore: move feature folder

--- server/feature/productSearch/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { deepEqual } from 'node:assert/strict'
-import { productSearch } from "node:test"
-productSearch('it works', () => {
-    deepEqual(0, 0);
-})
---
+++ server/src/feature/productSearch/index.test.ts
@@ -0,0 +1,5 @@
+import { deepEqual } from 'node:assert/strict'
+import { test } from "node:test"
+test('it works', () => {
+    deepEqual(0, 0);
+})
